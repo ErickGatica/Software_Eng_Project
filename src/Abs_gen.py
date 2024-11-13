5963c3b8013b5f2e31c76b68dbfed8e5f5fed7af
ErickGatica <erga9625@colorado.edu>
2024-11-12 23:26:57 -0700

    some updates in the GUI and generation of spectra

--- src/Abs_gen.py
+++ src/Abs_gen.py
@@ -9,9 +9,129 @@ The absorption spectra will be generated using the HITRAN database and the HITRA
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 from hapi import *
 
 # Impor the variables from the Variables.py script
 from Variables import args
 from Variables import args_dict
+
+# Constant
+R=8.31446261815324 #J/(mol K)
+
 # Functions
+
+def spectrum(P,T,length,numin,numax,molecule_id,isotopo_id,method_,wavestep,molar):
+
+    molecule=moleculeName(molecule_id); #Getting the name of the molecule from the id
+    #if isotopo_id is None:
+    #    isotopo_id = 1
+
+    name_isoto=isotopologueName(molecule_id,isotopo_id); #Getting the name of the isotopologue from the id
+
+    fetch(molecule,molecule_id,isotopo_id,numin,numax)
+    # Lets go with the data
+    if method_=="HT":
+        nu,coef=absorptionCoefficient_HT(SourceTables=molecule,WavenumberStep=wavestep,Diluent={'self':molar,'air':1-molar},Environment={'T':T,'p':P,'l':length})
+    elif method_=="V":
+        nu,coef=absorptionCoefficient_Voigt(SourceTables=molecule,WavenumberStep=wavestep,Diluent={'self':molar,'air':1-molar},Environment={'T':T,'p':P,'l':length})
+    elif method_=="L":
+        nu,coef=absorptionCoefficient_Lorentz(SourceTables=molecule,WavenumberStep=wavestep,Diluent={'self':molar,'air':1-molar},Environment={'T':T,'p':P,'l':length})
+    elif method_=="D":
+        nu,coef=absorptionCoefficient_Doppler(SourceTables=molecule,WavenumberStep=wavestep,Diluent={'self':molar,'air':1-molar},Environment={'T':T,'p':P,'l':length})
+
+#nu,coef=absorptionCoefficient_Gauss(SourceTables=molecule,Diluent={'air':1.0},Environment={'T':T,'p':P,'l':length})
+#-> absorptionCoefficient_HT
+#-> absorptionCoefficient_Voigt
+#-> absorptionCoefficient_Lorentz
+#-> absorptionCoefficient_Doppler
+
+    #Getting the spectrums
+    # Lets go with absorption Spectrum
+    nu,absorp=absorptionSpectrum(nu,coef)
+    # Lets go with transmittance Spectrum
+    nu,trans=transmittanceSpectrum(nu,coef)
+    # Lets go with radiance Spectrum
+    nu,radi = radianceSpectrum(nu,coef)
+
+    # Transforming the coef data to absorption
+    
+
+    # Lets define a class to save the data
+    class Data_spectrum:
+        def __init__(self, nu, coef, absorp, trans, radi,name_isoto):
+            self.nu = nu
+            self.coef = coef
+            self.absorp = absorp
+            self.trans = trans
+            self.radi = radi
+            self.name_isoto=name_isoto
+    
+    # Set the font to resemble LaTeX
+    #plt.rcParams['text.usetex'] = True
+    #plt.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
+        '''
+        # Creating the Figure 1 coefficient
+        fig1, ax1 = plt.subplots()
+        # Creating the plot
+        ax1.plot(nu,coef)
+        # Add a legend
+        ax1.legend(loc='upper right')
+        # Label the axes
+        ax1.set_xlabel(r'$\nu$ $cm^{-1}$')
+        ax1.set_ylabel(r'Coefficient $cm^2/molecule$')
+        # Title
+        ax1.set_title('Absorption coefficient for '+ name_isoto+ '$T$='+str(T)+'K $P$='+str(P)+'atm')
+
+        # Creating the Figure 2 absorption spectrum
+        fig2, ax2 = plt.subplots()
+        # Creating the plot
+        ax2.plot(nu,absorp)
+        # Add a legend
+        ax2.legend(loc='upper right')
+        # Label the axes
+        ax2.set_xlabel(r'$\nu$ $cm^{-1}$')
+        ax2.set_ylabel(r'Absorbance')
+        # Title
+        ax2.set_title('Absorption spectrum for '+ name_isoto+ '$T$='+str(T)+'K $P$='+str(P)+'atm')
+
+        # Creating the Figure 3 transmittance spectrum
+        fig3, ax3 = plt.subplots()
+        # Creating the plot
+        ax3.plot(nu,trans)
+        # Add a legend
+        ax3.legend(loc='upper right')
+        # Label the axes
+        ax3.set_xlabel(r'$\nu$ $cm^{-1}$')
+        ax3.set_ylabel(r'Transmittance')
+        # Title
+        ax3.set_title('Transmittance spectrum for '+ name_isoto+ '$T$='+str(T)+'K $P$='+str(P)+'atm')
+
+        # Creating the Figure 4 radiance spectrum
+        fig4, ax4 = plt.subplots()
+        # Creating the plot
+        ax4.plot(nu,radi)
+        # Add a legend
+        ax4.legend(loc='upper right')
+        # Label the axes
+        ax4.set_xlabel(r'$\nu$ $cm^{-1}$')
+        ax4.set_ylabel(r'Radiance')
+        # Title
+        ax4.set_title('Radiance spectrum for '+ name_isoto+ '$T$='+str(T)+'K $P$='+str(P)+'atm')
+        #Showing the plot
+        plt.show()
+        '''
+
+    return Data_spectrum(nu, coef, absorp, trans, radi, name_isoto)
+
+#spectrum(P,T,length,numin,numax,molecule_id,isotopo_id)
+
+
+# Function to plot using the data that was generated
+
+def plot_spectra(data):
+    # Set the font to resemble LateX
+    fig1, ax1 = plt.subplots()
+    # Creating the plot
+    ax1.plot(data.nu,data.coef)
+    
--- src/GUI_gen.py
+++ src/GUI_gen.py
@@ -5,7 +5,11 @@ It uses the pyqt5 library to generate the GUI
 """
 # Libraries
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QTabWidget, QLabel, QLineEdit, QPushButton, QHBoxLayout, QComboBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget,QGroupBox, QVBoxLayout, QTabWidget, QLabel, QLineEdit, QPushButton, QHBoxLayout, QComboBox, QFrame
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
 
 # Importing the variables from the Variables.py script
 # from Variables import args
@@ -34,42 +38,10 @@ class GUI(QMainWindow):
         self.init_lookuptable_tab()
 
     def init_absorption_tab(self):
-        layout = QVBoxLayout()
+        main_layout = QHBoxLayout()
 
-        temp_layout = QHBoxLayout()
-        self.temp_input = QLineEdit()
-        self.temp_label = QLabel("Temperature (K)")
-        temp_layout.addWidget(self.temp_input)
-        temp_layout.addWidget(self.temp_label)
-        layout.addLayout(temp_layout)
-
-        pressure_layout = QHBoxLayout()
-        self.pressure_input = QLineEdit()
-        self.pressure_label = QLabel("Pressure (atm)")
-        pressure_layout.addWidget(self.pressure_input)
-        pressure_layout.addWidget(self.pressure_label)
-        layout.addLayout(pressure_layout)
-
-        wavenumber_min_layout = QHBoxLayout()
-        self.wavenumber_min_input = QLineEdit()
-        self.wavenumber_min_label = QLabel("Minimum Wavenumber (cm-1)")
-        wavenumber_min_layout.addWidget(self.wavenumber_min_input)
-        wavenumber_min_layout.addWidget(self.wavenumber_min_label)
-        layout.addLayout(wavenumber_min_layout)
-
-        wavenumber_max_layout = QHBoxLayout()
-        self.wavenumber_max_input = QLineEdit()
-        self.wavenumber_max_label = QLabel("Maximum Wavenumber (cm-1)")
-        wavenumber_max_layout.addWidget(self.wavenumber_max_input)
-        wavenumber_max_layout.addWidget(self.wavenumber_max_label)
-        layout.addLayout(wavenumber_max_layout)
-
-        wavenumber_step_layout = QHBoxLayout()
-        self.wavenumber_step_input = QLineEdit()
-        self.wavenumber_step_label = QLabel("Wavenumber Step (cm-1)")
-        wavenumber_step_layout.addWidget(self.wavenumber_step_input)
-        wavenumber_step_layout.addWidget(self.wavenumber_step_label)
-        layout.addLayout(wavenumber_step_layout)
+        input_group_box = QGroupBox("Input Parameters")
+        input_layout = QVBoxLayout()
 
         molecule_layout = QHBoxLayout()
         self.molecule_input = QComboBox()
@@ -77,13 +49,70 @@ class GUI(QMainWindow):
         self.molecule_label = QLabel("Molecule:")
         molecule_layout.addWidget(self.molecule_input)
         molecule_layout.addWidget(self.molecule_label)
-        layout.addLayout(molecule_layout)
+        input_layout.addLayout(molecule_layout)
+
+        temp_layout = QHBoxLayout()
+        self.temp_input = QLineEdit()
+        self.temp_label = QLabel("Temperature (K)")
+        temp_layout.addWidget(self.temp_input)
+        temp_layout.addWidget(self.temp_label)
+        input_layout.addLayout(temp_layout)
+
+        pressure_layout = QHBoxLayout()
+        self.pressure_input = QLineEdit()
+        self.pressure_label = QLabel("Pressure (atm)")
+        pressure_layout.addWidget(self.pressure_input)
+        pressure_layout.addWidget(self.pressure_label)
+        input_layout.addLayout(pressure_layout)
+
+        molar_fraction_layout = QHBoxLayout() 
+        self.molar_fraction_input = QLineEdit()
+        self.molar_fraction_label = QLabel("Molar Fraction")
+        molar_fraction_layout.addWidget(self.molar_fraction_input)
+        molar_fraction_layout.addWidget(self.molar_fraction_label)
+        input_layout.addLayout(molar_fraction_layout)
+
+        length_layout = QHBoxLayout()
+        self.length_input = QLineEdit()
+        self.length_label = QLabel("Path Length (cm)")
+        length_layout.addWidget(self.length_input)
+        length_layout.addWidget(self.length_label)
+        input_layout.addLayout(length_layout)
+
+        wavenumber_min_layout = QHBoxLayout()
+        self.wavenumber_min_input = QLineEdit()
+        self.wavenumber_min_label = QLabel("Minimum Wavenumber (cm-1)")
+        wavenumber_min_layout.addWidget(self.wavenumber_min_input)
+        wavenumber_min_layout.addWidget(self.wavenumber_min_label)
+        input_layout.addLayout(wavenumber_min_layout)
+
+        wavenumber_max_layout = QHBoxLayout()
+        self.wavenumber_max_input = QLineEdit()
+        self.wavenumber_max_label = QLabel("Maximum Wavenumber (cm-1)")
+        wavenumber_max_layout.addWidget(self.wavenumber_max_input)
+        wavenumber_max_layout.addWidget(self.wavenumber_max_label)
+        input_layout.addLayout(wavenumber_max_layout)
+
+        wavenumber_step_layout = QHBoxLayout()
+        self.wavenumber_step_input = QLineEdit()
+        self.wavenumber_step_label = QLabel("Wavenumber Step (cm-1)")
+        wavenumber_step_layout.addWidget(self.wavenumber_step_input)
+        wavenumber_step_layout.addWidget(self.wavenumber_step_label)
+        input_layout.addLayout(wavenumber_step_layout)
+
 
         self.generate_button = QPushButton("Generate Absorption Spectra")
         self.generate_button.clicked.connect(self.generate_absorption_spectra)
-        layout.addWidget(self.generate_button)
+        input_layout.addWidget(self.generate_button)
 
-        self.absorption_tab.setLayout(layout)
+        input_group_box.setLayout(input_layout)
+        main_layout.addWidget(input_group_box)
+
+        # Placeholder for the plot area
+        self.plot_canvas = FigureCanvas(Figure())
+        main_layout.addWidget(self.plot_canvas)
+
+        self.absorption_tab.setLayout(main_layout)
 
     def init_fitting_tab(self):
         layout = QVBoxLayout()
